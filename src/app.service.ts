f03685300e86ad2be460b84eb9e371a61bc4df1e
Phil Kipkemboi <kipkemboiphil@gmail.com>
2026-06-20 12:34:31 +0300

    Add todo reminder to the healthcheck endpoint

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,13 +4,14 @@ import { FIRESTORE } from './config/firebase.constants';
 
 @Injectable()
 export class AppService {
-  constructor(@Inject(FIRESTORE) private readonly firestore: Firestore) {}
+  constructor(@Inject(FIRESTORE) private readonly firestore: Firestore) { }
 
   getHello(): string {
     return 'Hello World!';
   }
 
   async checkFirestore() {
+    // TODO improve on the health check later. Return more usefull info like the storage used etc. Send via mail.
     const ref = await this.firestore.collection('_health').add({ checkedAt: Timestamp.now() });
     return { ok: true, docId: ref.id };
   }
